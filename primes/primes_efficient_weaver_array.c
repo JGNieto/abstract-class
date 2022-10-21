feef89be02b1796977c1e882a8ffed857a86247c
Javier Garcia <javier@beren.dev>
2022-10-21 14:19:37 -0400

    Even more efficiency.

--- primes/primes_efficient_weaver_array.c
+++ primes/primes_efficient_weaver_array.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ARRAY_SIZE 1000000
 #define MAX 1000000
+#define ARRAY_SIZE 1000000
 
 int main() {
   int primes[ARRAY_SIZE];
   primes[0] = 2;
+  primes[1] = 3;
 
   int index = 1;
 
   for (int i = 3; i < MAX; i += 2) {
     int found_factor = 0;
-    for (int j = 3; j * j <= i; j += 2) {
-      if (i % j == 0) {
+    int j = 1;
+    int n = primes[j];
+    while (n * n <= i) {
+      if (i % n == 0) {
         found_factor = 1;
         break;
       }
+      n = primes[++j];
     }
 
     if (index >= ARRAY_SIZE) {
@@ -31,7 +35,7 @@ int main() {
   printf("There are %d prime numbers.", index);
 
   FILE *fptr;
-  fptr = fopen("./primes_efficient_weaver.txt","w");
+  fptr = fopen("./primes_efficient_weaver_array.txt","w");
 
   if (fptr == NULL) {
     printf("Error!");
